8e732587e53b6db935e84c713db361369e21fec8
sunleishe <249415876@qq.com>
2020-08-06 23:02:08 +0800

    测试第一次提交

---
+++ sunleishe/黑客攻击系统.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+int main (void){
+
+	string name;
+
+	cout << "ÇëÊäÈëÄúµÄÕËºÅ:" << endl;
+	cin >> name;
+
+
+}
